9f26d8c60c2bdf462ebb86fcf3f1b823d2f5e11e
EsranurGalipKayahan <61011249+EsranurGalipKayahan@users.noreply.github.com>
2021-10-07 06:27:13 +0200

    Update constants.js

--- sources/constants.js
+++ sources/constants.js
@@ -3,10 +3,12 @@ export const BAD_REQUEST_CODE = 400;
 export const NOT_FOUND_CODE = 404;
 export const SUCCESSFUL_OPERATION_CODE = 200;
 export const NOT_FOUND_MESSAGE = "Not found movie with id :";
-export const BAD_REQUEST_MESSAGE = "Missing attribute(s) for the operation!";
+export const BAD_REQUEST_MESSAGE =
+  "You have to send title, director and release_date of the movie!";
 export const SUCCESSFUL_OPERATION_MESSAGE =
-  " operation is successfully done for movie witd id : ";
+  " operation is successfully done for movie with id : ";
 export const CREATE_OPERATION_NAME = "Create ";
 export const DELETE_OPERATION_NAME = "Delete";
 export const PORT = 3000;
 export const PATH_ERROR_MESSAGE = "Wrong Path!";
+export const MISSING_ATTRIBUTE_MESSAGE = " attribute is missing for the movie!";
